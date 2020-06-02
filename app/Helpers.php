039708d7fb29f87ac659db27f129b9bf868404a6
administrator@IWDT.LOCAL <administrator@IWDT.LOCAL>
2020-06-02 21:10:53 +0300

    logging details

--- app/Helpers.php
+++ app/Helpers.php
@@ -9,7 +9,7 @@ class Helpers{
         $transport->setRecvTimeout(10000);
         $transport->setSendTimeout(10000);
 
-        $smpp = new SmppClient($transport);
+        $smpp = new SmppClient($transport, 'printDebug');
 
         // Activate binary hex-output of server interaction
         $smpp->debug = true;
@@ -34,6 +34,9 @@ class Helpers{
 
     public static function wh_log($log_msg)
     {
+
+        $now = '[' . date("Y-m-d H:i:s") . ']';
+
         $log_filename = "log";
         if (!file_exists($log_filename)) 
         {
@@ -42,7 +45,7 @@ class Helpers{
         }
         $log_file_data = $log_filename.'/log_' . date('d-M-Y') . '.log';
         // if you don't add `FILE_APPEND`, the file will be erased each time you add a log
-        file_put_contents($log_file_data, $log_msg . "\n", FILE_APPEND);
+        file_put_contents($log_file_data, $now . ' ' . $log_msg . "\n", FILE_APPEND);
     } 
 
 }
--- app/SMPPDaemon.php
+++ app/SMPPDaemon.php
@@ -5,7 +5,15 @@ use React\Http\Response;
 use Psr\Http\Message\ServerRequestInterface;
 
 function printDebug($str) {
-		echo date('Ymd H:i:s ').$str."\r\n";
+    $log_filename = "log";
+    if (!file_exists($log_filename)) 
+    {
+        // create directory/folder uploads.
+        mkdir($log_filename, 0777, true);
+    }
+    $log_file_data = $log_filename.'/log_low_' . date('d-M-Y') . '.log';
+    // if you don't add `FILE_APPEND`, the file will be erased each time you add a log
+    file_put_contents($log_file_data, $str . "\n", FILE_APPEND);
 }
 
 $smpp = Helpers::getSMPPConnection();
@@ -38,11 +46,13 @@ $server = new Server(function (ServerRequestInterface $request) use( &$smpp ) {
       );
   }
 
-  Helpers::wh_log('Send Request');
+
 
   $sender = $queryParams['senderNumber'];
   $receiver = $queryParams['receiverNumber'];
   $message = $queryParams['message'];
+
+  Helpers::wh_log('Send Request To Number ' . $receiver );
   
   $encodedMessage = mb_convert_encoding($message,'UTF-8','UCS-2');
   $encodedMessage = iconv('utf-8', "UCS-2BE", $message);
