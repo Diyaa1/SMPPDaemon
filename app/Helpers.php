1836748ef07566d6343c9917c44aa7e1324f5259
administrator@IWDT.LOCAL <administrator@IWDT.LOCAL>
2020-06-17 16:39:42 +0300

    revert

--- app/Helpers.php
+++ app/Helpers.php
@@ -34,15 +34,18 @@ class Helpers{
 
     public static function wh_log($log_msg)
     {
+
+        $now = '[' . date("Y-m-d H:i:s") . ']';
+
         $log_filename = "log";
         if (!file_exists($log_filename)) 
         {
             // create directory/folder uploads.
             mkdir($log_filename, 0777, true);
         }
-        $log_file_data = $log_filename.'/log_low_' . date('d-M-Y') . '.log';
+        $log_file_data = $log_filename.'/log_' . date('d-M-Y') . '.log';
         // if you don't add `FILE_APPEND`, the file will be erased each time you add a log
-        file_put_contents($log_file_data, $str . "\n", FILE_APPEND);
+        file_put_contents($log_file_data, $now . ' ' . $log_msg . "\n", FILE_APPEND);
     } 
 
 }
